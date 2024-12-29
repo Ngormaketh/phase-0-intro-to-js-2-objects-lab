771b14de6d51c5daf18a5d5603da375d3a155911
Anyang Diing <anyang.ngor@student.moringaschool.com>
2024-12-29 18:13:02 +0300

    Introduction to JavaScript Objects

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+// Employee object
+const employee = {
+    name: "Sam"
+  };
+  
+  // Non-destructive function to update the employee object
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  }
+  
+  // Destructive function to update the employee object
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Non-destructive function to delete a key from the employee object
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Destructive function to delete a key from the employee object
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey,
+  };
+  
